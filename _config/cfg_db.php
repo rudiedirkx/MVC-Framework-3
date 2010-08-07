10cdf05d0163571410a7467658483d6cfadf842a
Rudie Dirkx <github@hotblocks.nl>
2010-08-07 23:19:05 +0200

    removed database details

--- _config/cfg_db.php
+++ _config/cfg_db.php
@@ -1,16 +1,8 @@
 <?php
 
-# MySQL login vars #
-if ( false ) {
-	define( 'SQL_HOST',				'xx' );
-	define( 'SQL_DB',				'xx' );
-	define( 'SQL_USER',				'xx' );
-	define( 'SQL_PASS',				'xx' );
-} else {
-	define( 'SQL_HOST',				'localhost' );
-	define( 'SQL_DB',				'tests' );
-	define( 'SQL_USER',				'usager' );
-	define( 'SQL_PASS',				'usager' );
-}
+define( 'SQL_HOST',				'localhost' );
+define( 'SQL_DB',				'tests' );
+define( 'SQL_USER',				'usager' );
+define( 'SQL_PASS',				'usager' );
 
-?>
+
